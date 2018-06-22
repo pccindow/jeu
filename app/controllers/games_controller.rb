167bb32c846ed589295d16b2a7b3bb660b3aa88e
callac <pccindo78@msn.com>
2018-06-22 12:16:08 +0200

    title_changed

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,23 @@
+require 'net/http'
+require 'json'
+require 'ostruct'
+
 class GamesController < ApplicationController
   layout "application"
+
   def index
-    @countdown = { seconds: 60 }
+    parse_api
+    #@countdown = { seconds: 60 }
+    @words = { words: @arrayOfWords }
+  end
+
+  private
+
+  def parse_api
+    url = "https://api.datamuse.com/words?ml=chicken&sp=*"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    array = JSON.parse(response, object_class: OpenStruct)
+    @arrayOfWords = array.map{|elem| elem[:word]}
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import 'css/application.scss';
 import ReactOnRails from 'react-on-rails';
 import App from './app';
+//import FindWords from '../component/findwords'
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
